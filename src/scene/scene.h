e2524b71f34fec48b6143c6472d0ec507dbc7db7
Radek státní podnik <eidam30procent@gmail.com>
2025-08-28 10:37:51 +0200

    Objects now clickable
    Took 3 minutes

--- src/scene/scene.h
+++ src/scene/scene.h
@@ -14,8 +14,11 @@ class Scene : public IDrawGui {
 public:
 
     explicit Scene(const std::vector<std::shared_ptr<Mesh>>&& meshes, std::shared_ptr<Camera> camera, std::shared_ptr<Texture> sky = {nullptr})
-        : meshes_(std::move(meshes)), camera_(std::move(camera)), sky_(std::move(sky)) {}
+        : meshes_(std::move(meshes)), camera_(std::move(camera)), sky_(std::move(sky)) {
 
+        if (!meshes_.empty())
+            selectedObject_ = meshes_[0];
+    }
 
     [[nodiscard]] Camera& getCamera() const { return *camera_; }
     void setCamera(std::shared_ptr<Camera> camera) { std::swap(camera_, camera);}
@@ -38,7 +41,5 @@ private:
     std::shared_ptr<Camera> camera_{};
     std::shared_ptr<Texture> sky_{};
 
-
     std::shared_ptr<Mesh> selectedObject_{};
-    uint32_t selectedObjectIndex_{0};
 };
