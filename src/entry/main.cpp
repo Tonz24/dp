ae9408a0d83a3e7fd4989d6aa8b899ba7a7d20b6
Radek státní podnik <eidam30procent@gmail.com>
2025-09-29 11:31:41 +0200

    NEXT EVENT ESTIMATION WITH TEN MILION BUGS!
    Took 52 minutes

--- src/entry/main.cpp
+++ src/entry/main.cpp
@@ -7,9 +7,9 @@ int main() {
 
     auto cam = std::make_shared<Camera>(glm::vec3{0,0,2},glm::vec3{0,0,0});
     auto sky = TextureManager::getInstance()->registerResource("sky", "../assets/sky/lebombo_4k.exr", false, false);
-    auto scene = std::make_shared<Scene>(ModelLoader::loadModel("room/room.obj", false),cam,std::move(sky));
+    //auto scene = std::make_shared<Scene>(ModelLoader::loadModel("room/room.obj", false),cam,std::move(sky));
     //auto scene = std::make_shared<Scene>(ModelLoader::loadModel("living_room/living_room.obj", false),cam,std::move(sky));
-    //auto scene = std::make_shared<Scene>(ModelLoader::loadModel("cornell/cornell_full.obj", false),cam,std::move(sky));
+    auto scene = std::make_shared<Scene>(ModelLoader::loadModel("cornell/cornell_full.obj", false),cam,std::move(sky));
 
     Engine::getInstance().setScene(std::move(scene));
     Engine::getInstance().run();
